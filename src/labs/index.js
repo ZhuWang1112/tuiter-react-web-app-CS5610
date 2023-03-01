528673b3e8b080fc9ccda86fd84dcaac50ad0efe
ZhuWang <wang.zhu1@northeastern.edu>
2023-03-01 00:38:24 -0800

    A6 Challenge: add nav

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,9 +1,11 @@
 import Assignment6 from "./a6";
+import Nav from "../nav";
 
 
 function Labs() {
     return(
         <div>
+            <Nav/>
             <Assignment6/>
         </div>
     );
